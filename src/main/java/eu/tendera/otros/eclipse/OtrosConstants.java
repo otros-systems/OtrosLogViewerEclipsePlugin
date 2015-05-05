d047c7e7cbcff928733ac173fc1d05788c6b4c9e
k.otrebski <k.otrebski@ocado.com>
2015-05-05 07:19:40 +0200

    Change default port to 5987

--- src/main/java/eu/tendera/otros/eclipse/OtrosConstants.java
+++ src/main/java/eu/tendera/otros/eclipse/OtrosConstants.java
@@ -4,7 +4,7 @@ public final class OtrosConstants {
 
   public static final String ENABLED = "ENABLED";
   public static final String PORT = "PORT";
-  public static final Integer DEFAULT_PORT = 8080;
+  public static final Integer DEFAULT_PORT = 5987;
   public static final Boolean DEFAULT_ENABLED = Boolean.TRUE;
   
   public static final String OTROS_LOGO_16 = "logo16.png";
